1f03009613e829f6ce483b99d4d6e80a51f27866
Miguel Jaramillo <mjl997@hotmail.com>
2024-04-18 19:27:13 -0500

    pantalla y teclado finalizados

--- JS/index.js
+++ JS/index.js
@@ -1,5 +1,20 @@
 let teclas;
+let pantalla;
 
 window.onload = function(){
-    teclas = document.getElementsByClassName('Tecla');
+    pantalla = document.getElementById('pantalla');
+    teclas = document.getElementsByClassName('teclas');
+    agregarEventos(teclas);
+    pantalla.value = '';
+}
+
+function agregarEventos(teclas){
+    for (let i = 0; i < teclas.length; i++){
+        teclas[i].addEventListener('click', function(){
+            let valor = this.innerHTML;
+            if(pantalla.value.length<10){
+                pantalla.value += valor;
+            }
+        })
+    }
 }
